02f50d4a49ff7c0feecce45f78b02917dfc96dd6
qvinh2512 <qvinh2512@gmail.com>
2026-05-20 17:00:55 +0700

    Update page.tsx

--- app/admin/video/page.tsx
+++ app/admin/video/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { createClient } from '../../../lib/supabase/client'
+import { createClient } from '../../lib/supabase/client'
 
 type Video = {
   id: string
