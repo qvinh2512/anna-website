548886b3e9f9ae3f0da21c4c00f52ba3dc51b795
qvinh2512 <qvinh2512@gmail.com>
2026-05-21 15:32:30 +0700

    Update page.tsx

--- app/xa-hoi/[slug]/page.tsx
+++ app/xa-hoi/[slug]/page.tsx
@@ -1,1 +1,4 @@
-
+import PostDetail from '../../../components/PostDetail'
+export default function XaHoi({ params }: { params: { slug: string } }) {
+  return <PostDetail slug={params.slug} backHref="/xa-hoi" backLabel="Xã Hội" />
+}
