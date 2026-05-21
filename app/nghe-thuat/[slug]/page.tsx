a3385a992ceb3e33d091a65cd86b2059a3fca732
qvinh2512 <qvinh2512@gmail.com>
2026-05-21 15:30:26 +0700

    Update page.tsx

--- app/nghe-thuat/[slug]/page.tsx
+++ app/nghe-thuat/[slug]/page.tsx
@@ -1,1 +1,4 @@
-
+import PostDetail from '../../../components/PostDetail'
+export default function NgheThuat({ params }: { params: { slug: string } }) {
+  return <PostDetail slug={params.slug} backHref="/nghe-thuat" backLabel="Nghệ Thuật" />
+}
