7a17992a45bddc23b3fca81afbfbffb364dacd8f
qvinh2512 <qvinh2512@gmail.com>
2026-05-21 18:34:51 +0700

    Update page.tsx

--- app/nghe-thuat/page.tsx
+++ app/nghe-thuat/page.tsx
@@ -1,14 +1,4 @@
 import PostList from '../../components/PostList'
-
 export default function NgheThuat() {
-  return (
-    <PostList
-      categoryId={3}
-      basePath="nghe-thuat"
-      title="Nghệ Thuật"
-      emoji="🎨"
-      subtitle="Hội họa, triển lãm và thế giới màu sắc của Anna."
-      color="emerald"
-    />
-  )
+  return <PostList categoryId={3} basePath="nghe-thuat" title="Nghệ Thuật" emoji="🎨" subtitle="Hội họa, triển lãm và thế giới màu sắc của Anna." color="emerald" settingKey="menu_nghethuat" />
 }
