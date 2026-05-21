d31c0ccf3531489827f723546ac6fac9843c875a
qvinh2512 <qvinh2512@gmail.com>
2026-05-21 15:15:11 +0700

    Update page.tsx

--- app/admin/bai-viet/page.tsx
+++ app/admin/bai-viet/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { createClient } from '../../lib/supabase/client'
+import { createClient } from '../../../lib/supabase/client'
 
 type Post = {
   id: string
