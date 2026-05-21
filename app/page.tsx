37f988f7ccf571072af9ade8a3a11fc328ed716a
qvinh2512 <qvinh2512@gmail.com>
2026-05-21 18:39:30 +0700

    Remove import statement for PostList component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import PostList from '../../components/PostList'
 export default function XaHoi() {
   return <PostList categoryId={6} basePath="xa-hoi" title="Xã Hội" emoji="🌏" subtitle="Cộng đồng, từ thiện và các hoạt động xã hội của Anna." color="violet" settingKey="menu_xahoi" />
 }
