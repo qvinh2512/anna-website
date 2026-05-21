e0a2c3215cced5b2477965c8c0f76d17b6328fe5
qvinh2512 <qvinh2512@gmail.com>
2026-05-21 18:35:59 +0700

    Update page.tsx

--- app/hoc-thuat/page.tsx
+++ app/hoc-thuat/page.tsx
@@ -1,14 +1,4 @@
 import PostList from '../../components/PostList'
-
 export default function HocThuat() {
-  return (
-    <PostList
-      categoryId={4}
-      basePath="hoc-thuat"
-      title="Học Thuật"
-      emoji="📚"
-      subtitle="Hành trình học tập tại Sedbergh Việt Nam và Nhạc viện."
-      color="blue"
-    />
-  )
+  return <PostList categoryId={4} basePath="hoc-thuat" title="Học Thuật" emoji="📚" subtitle="Hành trình học tập tại Sedbergh Việt Nam và Nhạc viện." color="blue" settingKey="menu_hocthuat" />
 }
