cb0fba44b121824ccec0ad2837117007b41b0060
qvinh2512 <qvinh2512@gmail.com>
2026-05-21 20:19:28 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/lib/supabase/client'
+import { createClient } from '../lib/supabase/client'
 import HomeClient from './HomeClient'
 
 export default async function HomePage() {
