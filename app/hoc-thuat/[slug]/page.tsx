53406b493528b62b8f83ece52a77b5224e241b30
qvinh2512 <qvinh2512@gmail.com>
2026-05-21 15:31:06 +0700

    Update page.tsx

--- app/hoc-thuat/[slug]/page.tsx
+++ app/hoc-thuat/[slug]/page.tsx
@@ -1,1 +1,4 @@
-
+import PostDetail from '../../../components/PostDetail'
+export default function HocThuat({ params }: { params: { slug: string } }) {
+  return <PostDetail slug={params.slug} backHref="/hoc-thuat" backLabel="Học Thuật" />
+}
